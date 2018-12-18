133fbe3f8fc60a85d1b16d4eec3d510e54e6f00c
asmx1986 <sebastianm.alonso@gmail.com>
2018-12-17 22:08:14 -0300

    configurando instalacion linux

--- ejemplo-service-linux.sh
+++ ejemplo-service-linux.sh
@@ -7,7 +7,7 @@ case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR  >> PATH_TO_LOGS 2>&1& echo $! > $PID_PATH_NAME
+            nohup java -jar "$PATH_TO_JAR"  >> $PATH_TO_LOGS 2>&1& echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is already running ..."
@@ -32,7 +32,7 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
-            nohup java -jar $PATH_TO_JAR  >> PATH_TO_LOGS 2>&1& echo $! > $PID_PATH_NAME
+            nohup java -jar "$PATH_TO_JAR"  >> $PATH_TO_LOGS 2>&1& echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is not running ..."
